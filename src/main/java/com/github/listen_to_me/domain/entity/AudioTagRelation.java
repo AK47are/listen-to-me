f5fa7aed7a8f1b7fb8aef88274544a2e7222a229
hulunkunzao <2380381330@qq.com>
2026-03-25 14:55:33 +0800

    fix(后端): 修复中间表没有主键报错

--- src/main/java/com/github/listen_to_me/domain/entity/AudioTagRelation.java
+++ src/main/java/com/github/listen_to_me/domain/entity/AudioTagRelation.java
@@ -1,5 +1,7 @@
 package com.github.listen_to_me.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import lombok.Getter;
@@ -19,7 +21,7 @@ import lombok.Setter;
 public class AudioTagRelation implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    @TableId(type = IdType.INPUT)
     private Long audioId;
 
     private Long tagId;
