b81ce00781ab7b30d6ce2cf827175360b5f2d078
Ning001 <2825601439@qq.com>
2026-03-27 22:14:21 +0800

    feat(后端): 实现根据 AudioIdList 批量获取 AudioTagVOList 方法

--- src/main/java/com/github/listen_to_me/mapper/SysTagMapper.java
+++ src/main/java/com/github/listen_to_me/mapper/SysTagMapper.java
@@ -2,15 +2,11 @@ package com.github.listen_to_me.mapper;
 
 import com.github.listen_to_me.domain.entity.SysTag;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.listen_to_me.domain.vo.AudioTagVO;
+import org.apache.ibatis.annotations.Param;
 
-/**
- * <p>
- *  Mapper 接口
- * </p>
- *
- * @author kun
- * @since 2026-03-24
- */
+import java.util.List;
+
 public interface SysTagMapper extends BaseMapper<SysTag> {
-
+    List<AudioTagVO> selectTagsByAudioIds(@Param("audioIds") List<Long> audioIds);
 }
