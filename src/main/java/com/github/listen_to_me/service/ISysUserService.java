b019d065b068e5f2d9f71417053a965eb865eb20
Ning001 <2825601439@qq.com>
2026-03-28 11:05:24 +0800

    feat(后端): 实现 getProfile 接口，获取个人资料

--- src/main/java/com/github/listen_to_me/service/ISysUserService.java
+++ src/main/java/com/github/listen_to_me/service/ISysUserService.java
@@ -2,7 +2,9 @@ package com.github.listen_to_me.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.github.listen_to_me.domain.entity.SysUser;
+import com.github.listen_to_me.domain.vo.UserVO;
 
 public interface ISysUserService extends IService<SysUser> {
 
+    UserVO findProfile();
 }
