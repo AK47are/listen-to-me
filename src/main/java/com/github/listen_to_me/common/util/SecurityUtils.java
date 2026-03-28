78339c5dd821663572d440d1316d68644b166c66
Ning001 <2825601439@qq.com>
2026-03-28 11:04:39 +0800

    feat(后端): 封装 SecurityUtils 工具类，实现获取登录用户 id 方法

---
+++ src/main/java/com/github/listen_to_me/common/util/SecurityUtils.java
@@ -0,0 +1,20 @@
+package com.github.listen_to_me.common.util;
+
+import com.github.listen_to_me.common.exception.AuthException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtils {
+    /**
+     * 获取当前登录用户ID
+     */
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 判断认证对象非空 + 已认证
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthException("认证获取失败");
+        }
+
+        return (Long) authentication.getPrincipal();
+    }
+}
