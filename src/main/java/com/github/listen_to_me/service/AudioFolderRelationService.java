d727771de029fb76f03422493e22a8e28f1d2aa8
hulunkunzao <2380381330@qq.com>
2026-03-28 22:42:59 +0800

    feat(后端): 实现收藏/取消收藏音频接口

--- src/main/java/com/github/listen_to_me/service/AudioFolderRelationService.java
+++ src/main/java/com/github/listen_to_me/service/AudioFolderRelationService.java
@@ -1,7 +1,9 @@
 package com.github.listen_to_me.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.listen_to_me.domain.dto.FavoriteActionDTO;
 import com.github.listen_to_me.domain.entity.AudioFolderRelation;
 
 public interface AudioFolderRelationService extends IService<AudioFolderRelation> {
+    public void saveAudioAction(FavoriteActionDTO favoriteActionDTO);
 }
--- src/main/java/com/github/listen_to_me/service/impl/AudioFolderRelationServiceImpl.java
+++ src/main/java/com/github/listen_to_me/service/impl/AudioFolderRelationServiceImpl.java
@@ -1,5 +1,10 @@
 package com.github.listen_to_me.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.listen_to_me.common.exception.BaseException;
+import com.github.listen_to_me.domain.dto.FavoriteActionDTO;
 import com.github.listen_to_me.domain.entity.AudioFolderRelation;
 import com.github.listen_to_me.mapper.AudioFolderRelationMapper;
 import com.github.listen_to_me.service.AudioFolderRelationService;
@@ -9,5 +14,29 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class AudioFolderRelationServiceImpl extends ServiceImpl<AudioFolderRelationMapper, AudioFolderRelation> implements AudioFolderRelationService {
+    private final AudioFolderRelationMapper audioFolderRelationMapper;
+    @Override
+    public void saveAudioAction(FavoriteActionDTO favoriteActionDTO) {
+        Wrapper<AudioFolderRelation> wrapper = Wrappers.lambdaQuery(AudioFolderRelation.class)
+                .eq(AudioFolderRelation::getAudioId, favoriteActionDTO.getAudioId())
+                .eq(AudioFolderRelation::getFolderId, favoriteActionDTO.getFolderId());
+        if (favoriteActionDTO.getAction().equals("COLLECT")) {
+            if (audioFolderRelationMapper.selectOne(wrapper) != null) {
+                throw new BaseException(409,"音频已存在收藏");
+            }
+            AudioFolderRelation audioFolderRelation = new AudioFolderRelation();
+            audioFolderRelation.setAudioId(favoriteActionDTO.getAudioId());
+            audioFolderRelation.setFolderId(favoriteActionDTO.getFolderId());
+            audioFolderRelationMapper.insert(audioFolderRelation);
 
+        } else if (favoriteActionDTO.getAction().equals("UNCOLLECT")) {
+            if (audioFolderRelationMapper.selectOne(wrapper) == null) {
+                throw new BaseException(404,"音频不存在收藏");
+            }
+            audioFolderRelationMapper.delete(wrapper);
+
+        }else{
+            throw new BaseException("前端操作错误");
+        }
+    }
 }
