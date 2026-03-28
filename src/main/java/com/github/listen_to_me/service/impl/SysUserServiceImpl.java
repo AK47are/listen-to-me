530785103ede03112bfbeb312d20fc4cfce8d978
Ning001 <2825601439@qq.com>
2026-03-28 11:05:24 +0800

    feat(后端): 实现 getProfile 接口，获取个人资料

--- src/main/java/com/github/listen_to_me/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/github/listen_to_me/service/impl/SysUserServiceImpl.java
@@ -1,5 +1,11 @@
 package com.github.listen_to_me.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.github.listen_to_me.common.exception.BaseException;
+import com.github.listen_to_me.common.util.MinioUtils;
+import com.github.listen_to_me.common.util.SecurityUtils;
+import com.github.listen_to_me.domain.vo.UserVO;
+
 import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,4 +21,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements ISysUserService {
 
+    @Override
+    public UserVO findProfile() {
+        Long currId = SecurityUtils.getCurrentUserId();
+        log.debug("查询详情 - ID: {}", currId);
+        SysUser sysUser = this.getById(currId);
+        try {
+            sysUser.setAvatar(MinioUtils.getPresignedUrl(sysUser.getAvatar()));
+        } catch (Exception e) {
+            throw new BaseException("头像 url 获取异常！");
+        }
+        UserVO userVO = BeanUtil.copyProperties(sysUser, UserVO.class);
+        log.debug("查询详情 - ID: {}, 用户信息: {}", currId, userVO);
+        return userVO;
+    }
 }
