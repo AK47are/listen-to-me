7c6c113459343b1b91696e0d89ec0ab9b282d086
hulunkunzao <2380381330@qq.com>
2026-03-29 16:20:43 +0800

    feat(后端): 添加创建收藏夹接口

--- src/main/java/com/github/listen_to_me/service/SysUserFolderService.java
+++ src/main/java/com/github/listen_to_me/service/SysUserFolderService.java
@@ -1,7 +1,9 @@
 package com.github.listen_to_me.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.listen_to_me.domain.dto.FolderDTO;
 import com.github.listen_to_me.domain.entity.SysUserFolder;
 
 public interface SysUserFolderService extends IService<SysUserFolder> {
+    void createFolder(FolderDTO folderDTO);
 }
