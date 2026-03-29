a371641850a9278d21a23f6a2ff1c1cb47668961
hulunkunzao <2380381330@qq.com>
2026-03-29 16:20:43 +0800

    feat(后端): 添加创建收藏夹接口

--- src/main/java/com/github/listen_to_me/controller/user/UserController.java
+++ src/main/java/com/github/listen_to_me/controller/user/UserController.java
@@ -3,9 +3,12 @@ package com.github.listen_to_me.controller.user;
 import com.github.listen_to_me.common.Result;
 import com.github.listen_to_me.domain.dto.UserProfileUpdateDTO;
 import com.github.listen_to_me.domain.dto.FavoriteActionDTO;
+import com.github.listen_to_me.domain.dto.FolderDTO;
 import com.github.listen_to_me.domain.vo.UserVO;
 import com.github.listen_to_me.service.AudioFolderRelationService;
+import com.github.listen_to_me.service.IAudioInfoService;
 import com.github.listen_to_me.service.ISysUserService;
+import com.github.listen_to_me.service.SysUserFolderService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -14,17 +17,20 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "用户接口", description = "用户相关接口包含用户信息查询、用户信息更新等操作")
+
 @RestController
 @RequestMapping("/user")
 @AllArgsConstructor
+@Slf4j
+@Tag(name = "用户管理", description = "包含登录、注册、用户信息管理等接口")
 public class UserController {
-
+    private final AudioFolderRelationService audioFolderRelationService;
+    private final SysUserFolderService sysUserFolderService;
+    private final IAudioInfoService audioInfoService;
     private ISysUserService sysUserService;
-    private AudioFolderRelationService audioFolderRelationService;
 
     @Operation(summary = "获取当前登录用户资料")
     @GetMapping("/profile")
@@ -44,5 +50,12 @@ public class UserController {
     public Result<Void> saveAudioAction(@RequestBody FavoriteActionDTO favoriteActionDTO) {
         audioFolderRelationService.saveAudioAction(favoriteActionDTO);
         return Result.success();
+    }
+
+    @PostMapping("/favorite/folder")
+    @Operation(summary = "创建收藏夹")
+    public Result<String> saveFavoriteFolder(@RequestBody FolderDTO folderDTO) {
+        sysUserFolderService.createFolder(folderDTO);
+        return Result.success("创建收藏夹成功");
     }
 }
--- src/main/java/com/github/listen_to_me/service/impl/SysUserFolderServiceImpl.java
+++ src/main/java/com/github/listen_to_me/service/impl/SysUserFolderServiceImpl.java
@@ -1,13 +1,38 @@
 package com.github.listen_to_me.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.github.listen_to_me.common.exception.BaseException;
+import com.github.listen_to_me.common.util.SecurityUtils;
+import com.github.listen_to_me.domain.dto.FolderDTO;
+import com.github.listen_to_me.domain.entity.Folder;
 import com.github.listen_to_me.domain.entity.SysUserFolder;
+import com.github.listen_to_me.mapper.FolderMapper;
 import com.github.listen_to_me.mapper.SysUserFolderMapper;
 import com.github.listen_to_me.service.SysUserFolderService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 @Service
 @AllArgsConstructor
 public class SysUserFolderServiceImpl extends ServiceImpl<SysUserFolderMapper, SysUserFolder> implements SysUserFolderService {
-
+    private final FolderMapper folderMapper;
+    private final SysUserFolderMapper sysUserFolderMapper;
+    @Override
+    @Transactional
+    public void createFolder(FolderDTO folderDTO) {
+        Folder folder = BeanUtil.copyProperties(folderDTO, Folder.class);
+        LambdaQueryWrapper<Folder> wrapper = Wrappers.lambdaQuery(Folder.class)
+                .eq(Folder::getName, folder.getName());
+        if (folderMapper.selectOne(wrapper) != null) {
+            throw new BaseException(409, "收藏夹名称已存在");
+        }
+        folderMapper.insert(folder);
+        SysUserFolder sysUserFolder = new SysUserFolder();
+        sysUserFolder.setFolderId(folder.getId());
+        sysUserFolder.setUserId(SecurityUtils.getCurrentUserId());
+        sysUserFolderMapper.insert(sysUserFolder);
+    }
 }
