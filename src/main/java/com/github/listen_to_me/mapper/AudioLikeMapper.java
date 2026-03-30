105e90d8af4d7ad82dae515824e64c1d1d256d07
Ning001 <2825601439@qq.com>
2026-03-30 13:02:13 +0800

    feat(后端): 添加 AudioLike 的 Mapper 类以及 xml 文件

---
+++ src/main/java/com/github/listen_to_me/mapper/AudioLikeMapper.java
@@ -0,0 +1,7 @@
+package com.github.listen_to_me.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.listen_to_me.domain.entity.AudioLike;
+
+public interface AudioLikeMapper extends BaseMapper<AudioLike> {
+}
