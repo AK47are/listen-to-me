a2817a68eb12022a0f99b239a8b258a6f0e1b68f
AK47are <3318517070@qq.com>
2026-03-30 23:04:11 +0800

    feat(后端): 实现用户查询余额数据接口

--- src/main/java/com/github/listen_to_me/service/ISysUserService.java
+++ src/main/java/com/github/listen_to_me/service/ISysUserService.java
@@ -3,6 +3,7 @@ package com.github.listen_to_me.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.github.listen_to_me.domain.dto.UserProfileUpdateDTO;
 import com.github.listen_to_me.domain.entity.SysUser;
+import com.github.listen_to_me.domain.vo.BalanceVO;
 import com.github.listen_to_me.domain.vo.UserVO;
 
 public interface ISysUserService extends IService<SysUser> {
@@ -10,4 +11,12 @@ public interface ISysUserService extends IService<SysUser> {
     UserVO findProfile();
 
     void modifyProfile(UserProfileUpdateDTO updateDTO);
+
+    /**
+     * 获取用户余额统计信息
+     * 
+     * @param userId 用户ID
+     * @return 余额统计VO
+     */
+    BalanceVO getBalanceStats(Long userId);
 }
