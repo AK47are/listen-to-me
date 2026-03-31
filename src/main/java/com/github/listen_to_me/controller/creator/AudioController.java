0d5e457873f377bb9be3994c2b0d3e798931d8a7
AK47are <3318517070@qq.com>
2026-03-31 16:55:26 +0800

    style(后端): 格式化部分文件代码

--- src/main/java/com/github/listen_to_me/controller/creator/AudioController.java
+++ src/main/java/com/github/listen_to_me/controller/creator/AudioController.java
@@ -1,12 +1,5 @@
 package com.github.listen_to_me.controller.creator;
 
-import com.github.listen_to_me.common.Result;
-import com.github.listen_to_me.domain.dto.AudioDTO;
-import com.github.listen_to_me.domain.vo.AudioPublishVO;
-import com.github.listen_to_me.service.IAudioInfoService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
-import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.github.listen_to_me.common.Result;
+import com.github.listen_to_me.domain.dto.AudioDTO;
+import com.github.listen_to_me.domain.vo.AudioPublishVO;
+import com.github.listen_to_me.service.IAudioInfoService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+
 @AllArgsConstructor
 @RequestMapping("/creator/audio")
 @RestController("creatorAudioController")
 @Tag(name = "音频稿件管理", description = "包含上传音频、上传封面等接口")
 public class AudioController {
     private final IAudioInfoService audioInfoService;
+
     @PostMapping("/upload")
     @PreAuthorize("hasAuthority('audio:upload')")
     public Result<String> uploadAudio(MultipartFile audioFile) throws Exception {
