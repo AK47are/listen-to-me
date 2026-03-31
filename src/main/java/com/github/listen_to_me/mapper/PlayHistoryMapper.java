4c4d06b877d8fef19bf38232fb120d7e2d5e2bb5
Ning001 <2825601439@qq.com>
2026-03-31 10:18:40 +0800

    feat(后端): 实现历史相关接口
    实现记录播放进度接口
    实现获取播放进度接口
    实现分页查询历史记录接口

--- src/main/java/com/github/listen_to_me/mapper/PlayHistoryMapper.java
+++ src/main/java/com/github/listen_to_me/mapper/PlayHistoryMapper.java
@@ -1,7 +1,13 @@
 package com.github.listen_to_me.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.github.listen_to_me.domain.entity.PlayHistory;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.listen_to_me.domain.vo.AudioVO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * <p>
@@ -13,4 +19,18 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface PlayHistoryMapper extends BaseMapper<PlayHistory> {
 
+    @Select({
+            "SELECT * FROM audio_info ai ",
+            "INNER JOIN play_history ph ON ai.id = ph.audio_id ",
+            "WHERE ph.user_id = #{userId} ORDER BY ph.update_time DESC"
+    })
+    IPage<AudioVO> selectHistoryPage(Page<AudioVO> page, @Param("userId") Long userId);
+
+    @Insert("INSERT INTO play_history (user_id, audio_id, last_position, update_time) " +
+            "VALUES (#{history.userId}, #{history.audioId}, #{history.lastPosition}, NOW()) " +
+            "ON DUPLICATE KEY UPDATE " +
+            "last_position = VALUES(last_position), " +
+            "update_time = NOW()")
+    boolean insertOrUpdate(@Param("history") PlayHistory history);
 }
+
--- src/main/java/com/github/listen_to_me/service/impl/PlayHistoryServiceImpl.java
+++ src/main/java/com/github/listen_to_me/service/impl/PlayHistoryServiceImpl.java
@@ -1,20 +1,74 @@
 package com.github.listen_to_me.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.listen_to_me.common.enumeration.RedisKey;
+import com.github.listen_to_me.common.exception.BaseException;
+import com.github.listen_to_me.common.util.RedisUtils;
+import com.github.listen_to_me.common.util.SecurityUtils;
+import com.github.listen_to_me.domain.dto.HistoryProgressDTO;
+import com.github.listen_to_me.domain.entity.AudioInfo;
 import com.github.listen_to_me.domain.entity.PlayHistory;
+import com.github.listen_to_me.domain.query.PageQuery;
+import com.github.listen_to_me.domain.vo.AudioVO;
+import com.github.listen_to_me.mapper.AudioInfoMapper;
 import com.github.listen_to_me.mapper.PlayHistoryMapper;
 import com.github.listen_to_me.service.IPlayHistoryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author kun
- * @since 2026-03-24
- */
+
+@RequiredArgsConstructor
 @Service
 public class PlayHistoryServiceImpl extends ServiceImpl<PlayHistoryMapper, PlayHistory> implements IPlayHistoryService {
+    private final PlayHistoryMapper playHistoryMapper;
+    private final AudioInfoMapper audioInfoMapper;
 
+    @Override
+    public void addPlayHistory(HistoryProgressDTO historyProgressDTO) {
+        Long currId = SecurityUtils.getCurrentUserId();
+        Long audioId = historyProgressDTO.getAudioId();
+        AudioInfo audioInfo = audioInfoMapper.selectById(audioId);
+        if(audioInfo == null) {
+            throw new BaseException(404, "音频不存在");
+        }
+        if(audioInfo.getDuration() < historyProgressDTO.getLastPosition()){
+            throw new BaseException(400, "播放进度不能大于音频时长");
+        }
+        PlayHistory history = new PlayHistory();
+        history.setAudioId(audioId);
+        history.setUserId(currId);
+        history.setLastPosition(historyProgressDTO.getLastPosition());
+        String historySuffix = currId.toString() + ":" + audioId.toString();
+        RedisUtils.set(RedisKey.USER_HISTORY, historySuffix, historyProgressDTO.getLastPosition());
+        playHistoryMapper.insertOrUpdate(history);
+    }
+
+    @Override
+    public IPage<AudioVO> getHistoryPage(PageQuery pageQuery) {
+        Long currId = SecurityUtils.getCurrentUserId();
+        Page<AudioVO> page = new Page<>(pageQuery.getPageNum(), pageQuery.getPageSize());
+        return playHistoryMapper.selectHistoryPage(page, currId);
+    }
+
+    @Override
+    public Integer findPlayHistory(Long audioId) {
+        Long currId = SecurityUtils.getCurrentUserId();
+        String historySuffix = currId.toString() + ":" + audioId.toString();
+        Integer lastPosition = RedisUtils.get(RedisKey.USER_HISTORY, historySuffix);
+        if(lastPosition != null) {
+            return lastPosition;
+        }
+        // TODO 后续改成异步 Redis 方式，让数据库定时向 Redis 读取更新数据
+        PlayHistory history = playHistoryMapper.selectOne(Wrappers.lambdaQuery(PlayHistory.class)
+                .eq(PlayHistory::getUserId, currId)
+                .eq(PlayHistory::getAudioId, audioId));
+        if(history == null) {
+            return null;
+        }
+        RedisUtils.set(RedisKey.USER_HISTORY, historySuffix, history.getLastPosition());
+        return history.getLastPosition();
+    }
 }
