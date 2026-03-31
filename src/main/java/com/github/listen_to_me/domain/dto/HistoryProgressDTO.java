e9ea02f3062af604e9c0af4b8d484995baf421ce
Ning001 <2825601439@qq.com>
2026-03-31 10:16:49 +0800

    feat(后端): 封装 HistoryProgressDTO 类

---
+++ src/main/java/com/github/listen_to_me/domain/dto/HistoryProgressDTO.java
@@ -0,0 +1,16 @@
+package com.github.listen_to_me.domain.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+public class HistoryProgressDTO {
+    @NotNull(message = "音频ID不能为空")
+    private Long audioId;
+
+
+    @PositiveOrZero(message = "播放进度不能为负数")
+    @NotNull(message = "播放进度不能为空")
+    private Integer lastPosition;
+}
