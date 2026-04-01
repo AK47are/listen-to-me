f18d38b72d008c275ef1f805ee8602748530b61c
AK47are <3318517070@qq.com>
2026-04-01 17:55:23 +0800

    feat(后端): ConsultPageQuery 扩充开始/结束时间字段
    让用户端和创作者端支持时间查询

--- src/main/java/com/github/listen_to_me/domain/query/ConsultPageQuery.java
+++ src/main/java/com/github/listen_to_me/domain/query/ConsultPageQuery.java
@@ -1,5 +1,9 @@
 package com.github.listen_to_me.domain.query;
 
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,4 +15,12 @@ public class ConsultPageQuery extends PageQuery {
 
     @Schema(description = "订单状态（可选）", example = "PENDING_CONFIRM")
     private String status;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Schema(description = "开始时间（可选）", example = "2026-04-01 00:00:00")
+    private LocalDateTime startTime;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Schema(description = "结束时间（可选）", example = "2026-04-30 23:59:59")
+    private LocalDateTime endTime;
 }
