8a0f63c28b80ee98811c3e9d663927568b5aec25
Ning001 <2825601439@qq.com>
2026-04-02 17:56:38 +0800

    feat(后端): 实现分页查询评论接口

--- src/main/java/com/github/listen_to_me/controller/user/CommentController.java
+++ src/main/java/com/github/listen_to_me/controller/user/CommentController.java
@@ -1,16 +1,18 @@
 package com.github.listen_to_me.controller.user;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.github.listen_to_me.common.Result;
 import com.github.listen_to_me.domain.dto.CommentDTO;
+import com.github.listen_to_me.domain.query.CommentQuery;
+import com.github.listen_to_me.domain.vo.CommentVO;
 import com.github.listen_to_me.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.*;
+
 
 @Tag(name = "评论接口")
 @RequiredArgsConstructor
@@ -24,5 +26,11 @@ public class CommentController {
     public Result<Void> saveComment(@Valid @RequestBody CommentDTO commentDTO) {
         commentService.addComment(commentDTO);
         return Result.success();
+    }
+
+    @Operation(summary = "获取评论分页")
+    @GetMapping("/page")
+    public Result<IPage<CommentVO>> getCommentPage(@ParameterObject CommentQuery commentQuery) {
+        return Result.success(commentService.findCommentPage(commentQuery));
     }
 }
