7dde9fe3c0a22480e609a9d12001bc48ce65a92e
Ning001 <2825601439@qq.com>
2026-04-02 18:25:31 +0800

    feat(后端): 实现点赞/取消点赞接口

--- src/main/java/com/github/listen_to_me/service/CommentLikeService.java
+++ src/main/java/com/github/listen_to_me/service/CommentLikeService.java
@@ -1,7 +1,10 @@
 package com.github.listen_to_me.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.github.listen_to_me.domain.dto.CommentLikeDTO;
 import com.github.listen_to_me.domain.entity.CommentLike;
 
+
 public interface CommentLikeService extends IService<CommentLike> {
+    void likeComment(CommentLikeDTO commentLikeDTO);
 }
