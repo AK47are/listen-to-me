b311058f8bfaf807d102bd9f5615bb38c7dd77ce
AK47are <3318517070@qq.com>
2026-04-02 12:14:06 +0800

    refactor(后端): 为创作者 AudioController 添加 OpenAPI 注解
    方便在 OpenAPI 文档查看

--- src/main/java/com/github/listen_to_me/controller/creator/AudioController.java
+++ src/main/java/com/github/listen_to_me/controller/creator/AudioController.java
@@ -12,6 +12,8 @@ import com.github.listen_to_me.domain.dto.AudioDTO;
 import com.github.listen_to_me.domain.vo.AudioPublishVO;
 import com.github.listen_to_me.service.IAudioInfoService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 
@@ -24,19 +26,24 @@ public class AudioController {
 
     @PostMapping("/upload")
     @PreAuthorize("hasAuthority('audio:upload')")
-    public Result<String> uploadAudio(MultipartFile audioFile) throws Exception {
-
+    @Operation(summary = "上传音频", description = "上传 MP3 格式的音频文件")
+    public Result<String> uploadAudio(
+            @Parameter(description = "音频文件", required = true) MultipartFile audioFile) throws Exception {
         return Result.success(audioInfoService.uploadAudio(audioFile));
     }
 
     @PostMapping("/cover/upload")
     @PreAuthorize("hasAuthority('cover:upload')")
-    public Result<String> uploadCover(MultipartFile coverFile) throws Exception {
+    @Operation(summary = "上传封面", description = "上传 JPG、JPEG、PNG 格式的封面图片")
+    public Result<String> uploadCover(
+            @Parameter(description = "封面文件", required = true) MultipartFile coverFile) throws Exception {
         return Result.success(audioInfoService.uploadCover(coverFile));
     }
 
     @PostMapping
-    public Result<AudioPublishVO> saveAudio(@RequestBody AudioDTO audioDTO) throws Exception {
+    @Operation(summary = "发布音频稿件", description = "创建音频稿件，提交后进入转码流程")
+    public Result<AudioPublishVO> saveAudio(
+            @Parameter(description = "音频稿件信息", required = true) @RequestBody AudioDTO audioDTO) throws Exception {
         return Result.success(audioInfoService.saveAudio(audioDTO));
     }
 }
