3239e292fd1e30407907afbd9f3e2763bc0576e0
AK47are <3318517070@qq.com>
2026-04-04 18:35:55 +0800

    fix(后端): Folder 和 FolderVO 日期字段使用 LocalDateTime 类型
    Close #80

--- src/main/java/com/github/listen_to_me/domain/vo/FolderVO.java
+++ src/main/java/com/github/listen_to_me/domain/vo/FolderVO.java
@@ -1,12 +1,14 @@
 package com.github.listen_to_me.domain.vo;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
 public class FolderVO {
-      private String name;
-      private String description;
-      private Long audioCount;
-      private Long createTime;
     private Long id;
+    private String name;
+    private String description;
+    private Long audioCount;
+    private LocalDateTime createTime;
 }
