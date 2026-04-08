80087eaec550fa87db7a5ac3c97c5397503e3c59
AK47are <3318517070@qq.com>
2026-04-08 18:20:11 +0800

    fix(后端): 创建收藏夹接口全局检查收藏夹是否重名
    应该只检查当前用户收藏夹是否重名。由于单独提供了 folderId，因此即使重
    名也没有关系，而且重名检验需要多表查询。为了方便，这里直接不用检验重名了

--- src/main/java/com/github/listen_to_me/service/impl/SysUserFolderServiceImpl.java
+++ src/main/java/com/github/listen_to_me/service/impl/SysUserFolderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.github.listen_to_me.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.github.listen_to_me.common.exception.BaseException;
-import com.github.listen_to_me.common.exception.ConflictException;
 import com.github.listen_to_me.common.util.SecurityUtils;
 import com.github.listen_to_me.domain.dto.FolderDTO;
 import com.github.listen_to_me.domain.entity.Folder;
@@ -14,12 +18,9 @@ import com.github.listen_to_me.domain.vo.FolderVO;
 import com.github.listen_to_me.mapper.FolderMapper;
 import com.github.listen_to_me.mapper.SysUserFolderMapper;
 import com.github.listen_to_me.service.ISysUserFolderService;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import cn.hutool.core.bean.BeanUtil;
+import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
@@ -30,17 +31,12 @@ public class SysUserFolderServiceImpl extends ServiceImpl<SysUserFolderMapper, S
 
     @Override
     @Transactional
-    public void createFolder(FolderDTO folderDTO) {
+    public void createFolder(Long userId, FolderDTO folderDTO) {
         Folder folder = BeanUtil.copyProperties(folderDTO, Folder.class);
-        LambdaQueryWrapper<Folder> wrapper = Wrappers.lambdaQuery(Folder.class)
-                .eq(Folder::getName, folder.getName());
-        if (folderMapper.selectOne(wrapper) != null) {
-            throw new ConflictException("收藏夹名称已存在");
-        }
         folderMapper.insert(folder);
         SysUserFolder sysUserFolder = new SysUserFolder();
         sysUserFolder.setFolderId(folder.getId());
-        sysUserFolder.setUserId(SecurityUtils.getCurrentUserId());
+        sysUserFolder.setUserId(userId);
         sysUserFolderMapper.insert(sysUserFolder);
     }
 
