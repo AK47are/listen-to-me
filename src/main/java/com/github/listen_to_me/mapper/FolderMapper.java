1e5ca9578caf3fde82e5025039a2b75b16a94cbb
AK47are <3318517070@qq.com>
2026-04-08 19:00:47 +0800

    feat(后端): 添加更新收藏夹音频数量字段 Mapper 方法

--- src/main/java/com/github/listen_to_me/mapper/FolderMapper.java
+++ src/main/java/com/github/listen_to_me/mapper/FolderMapper.java
@@ -6,6 +6,11 @@ import com.github.listen_to_me.domain.entity.SysUserFolder;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Update;
+
 public interface FolderMapper extends BaseMapper<Folder> {
     List<Folder> getFolderListOfUsers(List<SysUserFolder> sysUserFolders);
+
+    @Update("UPDATE folder SET audio_count = audio_count + #{delta} WHERE id = #{folderId}")
+    void incrementAudioCount(Long folderId, int delta);
 }
