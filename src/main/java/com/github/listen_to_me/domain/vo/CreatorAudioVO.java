106378455c524c97a0ee04892921a4c555c79985
AK47are <3318517070@qq.com>
2026-04-09 20:49:46 +0800

    feat(后端): CreatorAudioVO 继承自 AudioVO 并调整相关接口
    主要调整创作者获取自己稿件的接口，让 CreatorAudioVO 只额外返回创作
    者必要字段 visibility, status 和 rejectReason

--- src/main/java/com/github/listen_to_me/domain/vo/CreatorAudioVO.java
+++ src/main/java/com/github/listen_to_me/domain/vo/CreatorAudioVO.java
@@ -2,20 +2,13 @@ package com.github.listen_to_me.domain.vo;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Schema(description = "创作者音频信息列表")
-public class CreatorAudioVO {
-    private  Long id;
-    private String title;
-    private String coverUrl;
-    private Long duration;
-    private Double price;
-    private Boolean isPaid;
+public class CreatorAudioVO extends AudioVO {
     private String visibility;
-    private  String status;
-    private Integer playCount;
-    private LocalDateTime createTime;
+    private String status;
+    private String rejectReason;
 }
